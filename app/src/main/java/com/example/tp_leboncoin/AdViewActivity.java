7832f1ccf9073726c4d9aad6d869ce3fe18a6f66
Martin <m.delsart67@gmail.com>
2024-02-21 11:56:48 +0100

    Commit Ajout Bouton Appeler - AddViewActivity

--- app/src/main/java/com/example/tp_leboncoin/AdViewActivity.java
+++ app/src/main/java/com/example/tp_leboncoin/AdViewActivity.java
@@ -1,18 +1,21 @@
 package com.example.tp_leboncoin;
 
 import android.content.Intent;
-import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
-import com.example.tp_leboncoin.sqlite.DBHelper;
 import com.example.tp_leboncoin.sqlite.DBManager;
 
 public class AdViewActivity extends AppCompatActivity {
+
+    private String phoneNumber; // Variable pour stocker le numéro de téléphone de l'annonce
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +47,30 @@ public class AdViewActivity extends AppCompatActivity {
                 titleTextView.setText(adModel.getTitle());
                 addressTextView.setText(adModel.getAddress());
                 phoneTextView.setText(adModel.getPhone()); // Afficher le numéro de téléphone
+
+                phoneNumber = adModel.getPhone(); // Stocker le numéro de téléphone dans la variable phoneNumber
             }
+        }
+    }
+
+    // Méthode pour composer le numéro de téléphone lorsque l'utilisateur clique sur le bouton d'appel
+    public void callPhoneNumber(View view) {
+        if (phoneNumber != null && !phoneNumber.isEmpty()) {
+            // Créer une intention d'appel
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:" + phoneNumber));
+
+            // Vérifier si l'application d'appel est disponible sur l'appareil
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                // Démarrer l'intention
+                startActivity(intent);
+            } else {
+                // Gérer le cas où aucune application d'appel n'est disponible
+                Toast.makeText(this, "Aucune application d'appel n'est disponible.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // Gérer le cas où le numéro de téléphone est vide ou null
+            Toast.makeText(this, "Numéro de téléphone non disponible.", Toast.LENGTH_SHORT).show();
         }
     }
 }
